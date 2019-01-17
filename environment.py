db6721ec4f0202a749e175625c0e9edf6f14d49c
Theo Kanning <theokanning@gmail.com>
2019-01-16 20:34:41 -0600

    Add train.py and add ability to draw environment

--- environment.py
+++ environment.py
@@ -1,7 +1,14 @@
 """This class represents a python Gridworld environment. Actions sent to this
 class will update the state of the environment and return a reward"""
 
-class Girdworld:
+AGENT='A'
+OPEN=' '
+OBSTACLE='#'
+GOAL='G'
+
+ACTIONS=['UP', 'DOWN', 'LEFT', 'RIGHT']
+
+class Gridworld:
 
   def __init__(self,
                step_cost=0.25,
@@ -13,10 +20,16 @@ class Girdworld:
          reward: The final reward for completing the world
     """
 
-    self._environment = []
     self._step_cost = step_cost
     self._obstacle_cost = obstacle_cost
     self._reward = reward
+    self._height = 3
+    self._width = 3
+    self._agentX = 0
+    self._agentY = 2
+    self._environment = [[OPEN, OPEN, GOAL],
+                         [OBSTACLE, OPEN, OBSTACLE],
+                         [OPEN, OPEN, OPEN]]
 
   def step(self, action):
     """Performs the given action and returns a reward, updated state, and a 
@@ -30,4 +43,15 @@ class Girdworld:
 
   def log(self):
     """Logs the current Gridworld state"""
-    pass
+    rowDivider = " _"*self._width
+    print rowDivider
+
+    for row in range(self._height): 
+      rowString = "|"
+      for cell in range(self._width):
+        if row == self._agentY and cell == self._agentX:
+	  rowString += AGENT
+        else:
+          rowString += self._environment[row][cell]
+        rowString += "|"
+      print rowString
---
+++ train.py
@@ -0,0 +1,5 @@
+import environment
+
+if __name__ == '__main__':
+  environment = environment.Gridworld()
+  environment.log()
