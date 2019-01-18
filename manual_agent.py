426ea2d080a64b326188d0ce2f21578e1f7d888d
Theo Kanning <theokanning@gmail.com>
2019-01-18 13:33:20 -0600

    Create separate manual and rl agents
    The manual agent might be useful to keep around

---
+++ manual_agent.py
@@ -0,0 +1,30 @@
+""" A simple Q-learning agent. Maintains an action-value matrix while exploring
+the gridworld. """
+
+import random
+
+class ManualAgent:
+
+  def __init__(self, num_actions):
+    """
+    Args:
+      num_actions: The number of available actions
+    """
+    self._num_actions = num_actions
+    
+  def _choose_action(self, state):
+    """Prompts the user to pick an action"""
+    return input("Choose action (0-{})".format(self._num_actions - 1))
+
+  def begin_episode(self, state):
+    """Begins a new training episode"""
+    return self._choose_action(state)
+
+  def end_episode(self, state):
+    """Marks the end of a training episode"""
+    pass
+
+  def step(self, reward, state):
+    """Performs a single step."""
+    return self._choose_action(state)
+
--- train.py
+++ train.py
@@ -1,9 +1,9 @@
-import agent
+import manual_agent
 import environment
 import runner
 
 if __name__ == '__main__':
-  agent = agent.RlAgent(4, 9)
+  agent = manual_agent.ManualAgent(4)
   environment = environment.Gridworld()
   runner = runner.Runner(agent, environment)
 
